4d88ae476001a91da8ae4461f59ca9d9d64d9f9e
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2025-03-29 11:36:37 +0200

    fixing some warnings and errors in the first fucntion documentation.

--- R/create_feature_list.R
+++ R/create_feature_list.R
@@ -18,6 +18,8 @@
 #' @param threshold An optional numeric vector (`float`) representing the threshold value below which
 #'                  input files are cut to zero.
 #' @param spp_file_dir A character string specifying the directory containing the raster files.
+#' @param recursive A logical value indicating whether to search subdirectories of `spp_file_dir`
+#'                  for raster files. Default is FALSE.
 #' @param spp_file_pattern A regular expression pattern used to filter the raster files
 #'                  matching `.tif` and `.img` files.
 #'
@@ -27,10 +29,12 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' create_feature_list(spp_file_dir = "path/to/raster/files",
 #'                     output_dir = "path/to/output",
 #'                     weight = c(0.1, 0.2, 0.3),
 #'                     threshold = 0.5)
+#' }
 create_feature_list <- function(filename = "feature_list.txt",
                                 output_dir = NULL,  # New parameter for specifying the folder
                                 weight = NULL,
