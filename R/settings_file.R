09a428122192ada565765f4e4ba2af68cd577428
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2026-01-07 14:07:50 -0300

    Update settings_file function and add tests

--- R/settings_file.R
+++ R/settings_file.R
@@ -4,16 +4,12 @@
 #' the input data and analysis configuration. The resulting settings
 #' file is saved with a `.z5` extension and can be used directly in the Zonation 5 software.
 #'
-#' @param zero_mode A character string specifying the zero mode parameter for Zonation.
 #' @param feature_list_file A character string specifying the feature list file.
 #'                  This is a compulsory parameter.
 #' @param external_solution_file A character string specifying the full path and/or name of the external solution file.
 #' @param analysis_area_mask_layer A character string specifying the full path and/or name of the analysis area mask layer file.
-#' @param weight_groups_file A character string specifying the full path and/or name of the weight groups file.
-#' @param condition_link_file A character string specifying the full path and/or name of the condition link file.
 #' @param hierarchic_mask_layer A character string specifying the full path and/or name of the hierarchic mask layer file.
 #' @param cost_layer A character string specifying the full path and/or name of the cost layer file.
-#' @param retention_link_file A character string specifying the full path and/or name of the retention link file.
 #'
 #' @returns A `.z5` file containing the specified settings.
 #'
@@ -21,58 +17,39 @@
 #'
 #' @examples
 #' \dontrun{
-#' create_settings_file(feature_list_file = "feature_list.txt")
+#' settings_file(feature_list_file = "feature_list.txt")
 #' }
-create_settings_file <- function(zero_mode = NULL,
-                                 feature_list_file = NULL,
-                                 external_solution_file = NULL,
-                                 analysis_area_mask_layer = NULL,
-                                 weight_groups_file = NULL,
-                                 condition_link_file = NULL,
-                                 hierarchic_mask_layer = NULL,
-                                 cost_layer = NULL,
-                                 retention_link_file = NULL) {
+settings_file <- function(feature_list_file,
+                          external_solution_file = NULL,
+                          analysis_area_mask_layer = NULL,
+                          hierarchic_mask_layer = NULL,
+                          cost_layer = NULL) {
 
   # Hardcoded filename
   filename <- "settings_file.z5"
 
   # Check if the feature_list_file is provided
   if (missing(feature_list_file)) {
-    stop("Missing feature_list_file. This is a compulsory parameter for running Zonation.")
+    stop("feature_list file is required and must be specified.")
   }
 
   # Open a connection to the file
   con <- file(filename, open = "wt")
 
-  # Write zero_mode to the settings file
-  if (!is.null(zero_mode)) {
-    writeLines(paste("zero mode =", zero_mode), con)
-  }
+    # Write file parameters to the settings file
+  writeLines(paste("feature list file =", feature_list_file), con)
 
-  # Write file parameters to the settings file
-  if (!is.null(feature_list_file)) {
-    writeLines(paste("feature list file =", feature_list_file), con)
-  }
   if (!is.null(external_solution_file)) {
     writeLines(paste("external solution =", external_solution_file), con)
   }
   if (!is.null(analysis_area_mask_layer)) {
     writeLines(paste("analysis area mask layer =", analysis_area_mask_layer), con)
   }
-  if (!is.null(weight_groups_file)) {
-    writeLines(paste("weight groups file =", weight_groups_file), con)
-  }
-  if (!is.null(condition_link_file)) {
-    writeLines(paste("condition link file =", condition_link_file), con)
-  }
   if (!is.null(hierarchic_mask_layer)) {
     writeLines(paste("hierarchic mask layer =", hierarchic_mask_layer), con)
   }
   if (!is.null(cost_layer)) {
     writeLines(paste("cost layer =", cost_layer), con)
-  }
-  if (!is.null(retention_link_file)) {
-    writeLines(paste("retention link file =", retention_link_file), con)
   }
 
   # Close the connection
